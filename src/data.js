18f9ba33b72055a7cd7edb24fbbfe42671c20401
palsev <palsev04157@gmail.com>
2023-07-27 16:42:18 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,151 @@
+import p2 from "./assets/project2.png";
+import p3 from "./assets/project3.png";
+import p5 from "./assets/project5.png";
+import p8 from "./assets/project8.png";
+import p11 from "./assets/project11.png";
+import p13 from "./assets/project13.png";
+import p15 from "./assets/project15.png";
+import p16 from "./assets/project16.png";
+import p17 from "./assets/project17.png";
+import p18 from "./assets/project18.png";
+import p19 from "./assets/project19.png";
+
+export const skills_list = [
+  { path : "./assets/SK1.svg" },
+  { path : "./assets/SK2.svg" },
+  { path : "./assets/SK3.svg" },
+  { path : "./assets/SK4.svg" },
+  { path : "./assets/SK5.svg" },
+  { path : "./assets/SK6.svg" },
+  { path : "./assets/SK7.svg" },
+  { path : "./assets/SK10.svg" },
+  { path : "./assets/SK11.svg" },
+  { path : "./assets/SK12.svg" },
+  { path : "./assets/SK13.svg" },
+  { path : "./assets/SK17.svg" },
+  { path : "./assets/SK18.svg" },
+  { path : "./assets/SK21.svg" },
+  { path : "./assets/SK23.svg" },
+  { path : "./assets/SK24.svg" },
+  { path : "./assets/SK25.svg" },
+  { path : "./assets/SK26.svg" },
+  { path : "./assets/SK27.svg" },
+  { path : "./assets/SK28.svg" },
+  { path : "./assets/SK30.svg" },
+  { path : "./assets/SK31.svg" },
+  { path : "./assets/SK33.svg" },
+  { path : "./assets/SK34.svg" },
+  { path : "./assets/SK35.svg" },
+  { path : "./assets/SK36.svg" },
+  { path : "./assets/SK37.svg" },
+  { path : "./assets/SK43.svg" },
+  { path: "./assets/Solidity.svg"},
+  { path: "./assets/Rust.svg"},
+]
+
+export const filters = [
+  {
+    name: "All",
+    id: "mine",
+  },
+  {
+    name: "FullStack",
+    id: "fullstack",
+  },
+  {
+    name: "Blockchain",
+    id: "Crypto",
+  },
+];
+
+export const projects = [
+  {
+    name      : "InfinityBit",
+    image     : p2,
+    tags      : ["mine", "fullstack"],
+    live_url  : "https://infinitybit.io/",
+  },
+  {
+    name      : "Geneis Vision",
+    image     : p3,
+    tags      : ["mine", "fullstack"],
+    live_url  : "https://genesis.vision/",
+  },
+  { 
+    name      : "Ocado Supermaket",
+    image     : p5,
+    tags      : ["mine", "fullstack"],
+    live_url  : "https://www.ocado.com",
+  },
+  { 
+    name      : "Bioceuticals",
+    image     : p11,
+    tags      : ["mine", "fullstack"],
+    live_url  : "https://www.bioceuticals.co.uk",
+  },  
+  { 
+    name      : "Medano",
+    image     : p13,
+    tags      : ["mine", "fullstack"],
+    live_url  : "https://medano.io/",
+  },
+  { 
+    name      : "Defibids",
+    image     : p8,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://defibids.com",
+  },
+  {  
+    name      : "XODEX",
+    image     : p15,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://www.xo-dex.com",
+  },
+  {  
+    name      : "Helio Protocol",
+    image     : p16,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://helio.money",
+  },
+  {  
+    name      : "Wanaka Farm",
+    image     : p17,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://wanakafarm.com",
+  },
+  {  
+    name      : "POLONIEX",
+    image     : p18,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://poloniex.com",
+  },
+  {  
+    name      : "TrillerNfts",
+    image     : p19,
+    tags      : ["mine", "Crypto"],
+    live_url  : "https://trillernfts.com",
+  },
+];
+
+export const achievements = [
+  {
+    id: 1,
+    year: 30,
+    description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+  },
+  {
+    id: 2,
+    year: 9,
+    description: "YEARS OF<br />EXPERIENCE",
+  },
+  {
+    id: 3,
+    year: 20,
+    description: "TECHNOLOGIES",
+  },
+  {
+    id: 4,
+    year: 50,
+    description: "COMMUNITY<br />POSTS",
+  },
+];
